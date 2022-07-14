28f9b6cef6dc92c63b2693fb186eea4191be6d19
Skyler Clark <skylerjaneclark@gmail.com>
2022-07-14 11:22:29 -0400

    fix

--- ci-operator/step-registry/crio-test/e2e/test/crio-test-e2e-test-commands.sh
+++ ci-operator/step-registry/crio-test/e2e/test/crio-test-e2e-test-commands.sh
@@ -41,8 +41,6 @@ timeout --kill-after 10m 120m ssh "${SSHOPTS[@]}" "root@${IP}" bash - << EOF
     cd "\${REPO_DIR}/contrib/test/ci"
     echo "localhost" >> hosts
     ansible-playbook e2e-main.yml -i hosts -e "TEST_AGENT=prow" -e "GOPATH=/usr/local/go" --connection=local -vvv
-    scp -r "${SSHOPTS[@]}" "root@${IP}:/tmp/artifacts/*" "${ARTIFACT_DIR}"
-    sleep 600
 EOF
 
 function getlogs() {
